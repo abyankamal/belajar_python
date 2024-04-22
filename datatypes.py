acf248d7c34e9732a660de123c4d814658a88e71
abyankamal <kamalmuhammad414@gmail.com>
2024-04-22 10:00:10 +0700

    hari pertama belajar python

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+# types data on python
+
+# String
+
+print("Hello, world!");
+
+# Integer
+
+print(1)
+
+# Float
+
+print(1.0)
+
+# Boolean
+
+print(True)
+print(False)
+
+# example of types data
+
+num_char = len(input("What Is Your Name ?"))
+new_num_char = str(num_char)
+print("Your Name Contains " + new_num_char + " characters")
+
+a = float(123)
+print(type(a))
+
+print(70 + float(100))
+print("70" + str(100))
---
+++ matematical.py
@@ -0,0 +1,21 @@
+# PEMDAS
+# ()
+# **
+# +
+# /
+# -
+
+print(3 ** 3)
+print(3 + 3)
+print(3 - 3)
+print(3 * 3)
+print(3 / 3)
+print(3 // 3)
+print(6 + 4 / 2 - (1 * 2))
+
+# fstring
+
+score = 0
+height = 1.8
+
+print(f"Your score is {score} and your height is {height}m")
