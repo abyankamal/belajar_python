e26eeb779413bb6520e2f6b01d551cf1c5cdffba
abyankamal <kamalmuhammad414@gmail.com>
2024-04-24 09:10:04 +0700

    hari ketiga belajar python

---
+++ lists.py
@@ -0,0 +1,29 @@
+names = ["Muhammad", "Abyan", "Kamal"]
+names.append("Udin")
+names.extend(["John", "Doe"])
+names.sort()
+print(names)
+print(len(names))
+
+buah = [
+    "apel",
+    "jeruk",
+    "pisang",
+    "mangga",
+    "anggur",
+    "nanas",
+    "stroberi",
+    "kiwi",
+    "melon",
+    "pepaya",
+    "semangka",
+    "markisa",
+    "sirsak",
+    "durian",
+    "rambutan",
+    "salak",
+    "lemon",
+    "ceri",
+    "blueberry",
+    "raspberry"
+]
