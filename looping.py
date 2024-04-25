3ad8aa81012225edd24f98c09ec7ede8b4d0bf70
abyankamal <kamalmuhammad414@gmail.com>
2024-04-25 10:55:49 +0700

    hari keempat belajar python

---
+++ looping.py
@@ -0,0 +1,15 @@
+count = 0
+fruits = ["Apel", "Banana", "Peach"]
+for fruit in fruits :
+    print(fruit)
+    print(len(fruit))
+
+# range function
+for number in range(1, 100, 3) :
+    print(number)
+
+total = 0
+for number in range(1, 101) :
+    total += number
+
+print(total)
