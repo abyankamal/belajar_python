29197375dc634900a982bd42a5feb86dff866c2e
abyankamal <kamalmuhammad414@gmail.com>
2024-05-04 11:48:13 +0700

    hari keduabelas belajar python

--- oop.py
+++ oop.py
@@ -10,13 +10,37 @@
 # print(screen.canvheight)
 # screen.exitonclick()
 
-from prettytable import PrettyTable
+# from prettytable import PrettyTable
 
-table = PrettyTable()
-table.add_column("Pokemon", ["Pikachu", "Charmander", "Squirtle"])
-table.add_column("Type", ["Electric", "Fire", "Water"])
+# table = PrettyTable()
+# table.add_column("Pokemon", ["Pikachu", "Charmander", "Squirtle"])
+# table.add_column("Type", ["Electric", "Fire", "Water"])
 
-table.align = "l"
+# table.align = "l"
 
-print(table)
+# print(table)
+
+# create our class
+class User :
+    # create constructor
+    def __init__(self, id, username) :
+        self.id = id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    # create method
+    def follow(self, user) :
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("001", "Angela")
+user_2 = User("002", "Budi")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
 
